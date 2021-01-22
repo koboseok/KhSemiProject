6ef9700bb07c6958b0f975630feeeafd208a722e
USER <USER@LAPTOP-BFVNQTFJ>
2021-01-22 15:37:15 +0900

    free 백

--- SemiProject/src/com/kh/semi/freeBoard/model/dao/FreeBoardDAO.java
+++ SemiProject/src/com/kh/semi/freeBoard/model/dao/FreeBoardDAO.java
@@ -1,8 +1,6 @@
+package com.kh.wsp.board.model.dao;
 
-package com.kh.semi.freeBoard.model.dao;
-
-import static com.kh.semi.common.JDBCTemplate.*;
-
+import static com.kh.wsp.common.JDBCTemplate.*;
 
 import java.io.FileInputStream;
 import java.sql.Connection;
@@ -14,221 +12,196 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import com.kh.semi.freeBoard.model.dao.FreeBoardDAO;
-import com.kh.semi.freeBoard.model.vo.FRAttachment;
-import com.kh.semi.freeBoard.model.vo.FreeBoard;
-import com.kh.semi.freeBoard.model.vo.FreePageInfo;
+import com.kh.wsp.board.model.vo.Attachment;
+import com.kh.wsp.board.model.vo.Board;
+import com.kh.wsp.board.model.vo.PageInfo;
 
-
-public class FreeBoardDAO {
-
+public class BoardDAO {
+	
 	private Statement stmt = null;
 	private PreparedStatement pstmt = null;
 	private ResultSet rset = null;
-
+	
 	private Properties prop = null;
 
-	// 기본 생성자
-	public FreeBoardDAO() {
-		String fileName = FreeBoardDAO.class.getResource(
-
-				"/com/kh/semi/sql/board/freeBoard-query.xml").getPath();
+	
+	public BoardDAO(){
+		String fileName = BoardDAO.class.getResource("/com/kh/wsp/sql/board/board-query.xml").getPath();
 		try {
 			prop = new Properties();
-			prop.loadFromXML(new FileInputStream(fileName));
+			prop.loadFromXML(new FileInputStream(fileName)); 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * 자유 게시판 글 수 반환DAO
+
+	/** 전체 게시글 수 반환 DAO
 	 * @param conn
-	 * @return fListCount
+	 * @return listCount
 	 * @throws Exception
 	 */
-
-	public int getfListCount(Connection conn) throws Exception {
-
-		int fListCount = 0;
-
-		String query = prop.getProperty("getfListCount");
-
+	public int getListCount(Connection conn) throws Exception {
+		int listCount = 0;
+		
+		String query = prop.getProperty("getListCount");
+		
 		try {
-
 			stmt = conn.createStatement();
-
+			
 			rset = stmt.executeQuery(query);
-
-			if (rset.next()) {
-				fListCount = rset.getInt(1);
+			
+			if(rset.next()) {
+				listCount = rset.getInt(1);
 			}
-
-		} finally {
-
-			close(stmt);
-
-		}
-
-		return fListCount;
-	}
-
-	/** 목록  조회 DAO
-	 * @param conn
-	 * @param fPInfo
-	 * @return
-	 */
-	public List<FreeBoard> selectFBoardList(Connection conn, FreePageInfo fPInfo) throws Exception {
-
-		List<FreeBoard> fList = null;
-
-		String query = prop.getProperty("selectFBoardList");
-
-
-
-
-
-		try {
-			// SQL 구문 조건절에 대입할 변수 생성
-			int startRow = (fPInfo.getCurrentPage() - 1) * fPInfo.getLimit() + 1; 
-			int endRow = startRow + fPInfo.getLimit() - 1; 
-
-			pstmt = conn.prepareStatement(query);
-
-			pstmt.setInt(1, startRow);
-			pstmt.setInt(2, endRow);
-
-			rset = pstmt.executeQuery();
-
-			fList = new ArrayList<FreeBoard>();
-
-
-			while (rset.next()) {
-				FreeBoard fBoard = new FreeBoard();
-
-				fBoard.setfBoardNo(rset.getInt("FR_NO"));
-				fBoard.setfBoardTitle(rset.getString("FR_TITLE"));
-				fBoard.setMemName(rset.getString("MEM_NM"));
-				fBoard.setfCreateDate(rset.getTimestamp("FR_C_DT"));
-				fBoard.setfReadCount(rset.getInt("FR_READ_COUNT"));
-				fBoard.setMemGrade(rset.getString("MEM_GRADE"));
-				fList.add(fBoard);
-			
-			
-			}
-			
 			
 		}finally {
-
 			close(rset);
-			close(pstmt);
-
-
+			close(stmt);
 		}
-
-		return fList;
+		
+		return listCount;
 	}
 
-	/** 게시글 상세 조회
+
+	/** 게시글 목록 조회 DAO
 	 * @param conn
-	 * @param fboardNo
-	 * @return fBoard
+	 * @param pInfo
+	 * @return bList
 	 * @throws Exception
 	 */
-	public FreeBoard selectFBoard(Connection conn, int fBoardNo)throws Exception {
-	
-		FreeBoard fBoard = null;
+	public List<Board> selectBoardList(Connection conn, PageInfo pInfo) throws Exception{
+		List<Board> bList = null;
 		
-		String query = prop.getProperty("selectFBoard");
+		String query = prop.getProperty("selectBoardList");
 		
 		try {
+			// SQL 구문 조건절에 대입할 변수 생성
+			int startRow = (pInfo.getCurrentPage() - 1) * pInfo.getLimit() + 1; 
+			int endRow = startRow + pInfo.getLimit() - 1; 
 			
 			pstmt = conn.prepareStatement(query);
 			
-			pstmt.setInt(1, fBoardNo);
+			pstmt.setInt(1, startRow);
+			pstmt.setInt(2, endRow);
+			
+			rset = pstmt.executeQuery();
+			
+			bList = new ArrayList<Board>();
+			
+			while(rset.next()) {
+				Board board = new Board(
+								rset.getInt("BOARD_NO"), 
+								rset.getString("BOARD_TITLE"), 
+								rset.getString("MEMBER_ID"), 
+								rset.getInt("READ_COUNT"), 
+								rset.getString("CATEGORY_NM"), 
+								rset.getTimestamp("BOARD_CREATE_DT"));
+				bList.add(board);
+			}
+			
+		}finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return bList;
+	}
+
+
+	/** 게시글 상세 조회 DAO
+	 * @param conn
+	 * @param boardNo
+	 * @return board
+	 * @throws Exception
+	 */
+	public Board selectBoard(Connection conn, int boardNo) throws Exception{
+		Board board = null;
+		
+		String query = prop.getProperty("selectBoard");
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setInt(1, boardNo);
 			
 			rset = pstmt.executeQuery();
 			
 			if(rset.next()) {
-				fBoard = new FreeBoard();
+				board = new Board();
 				
-				fBoard.setfBoardNo(rset.getInt("FR_NO"));
-				fBoard.setfBoardTitle(rset.getString("FR_TITLE"));
-				fBoard.setfBoardContent(rset.getString("FR_CONTENT"));
-				fBoard.setMemName(rset.getString("MEM_NM"));
-				fBoard.setfCreateDate(rset.getTimestamp("FR_C_DT"));
-				fBoard.setfReadCount(rset.getInt("FR_READ_COUNT"));
-				fBoard.setMemGrade(rset.getString("MEM_GRADE"));	
-				
+				board.setBoardNo( rset.getInt("BOARD_NO") );
+				board.setBoardTitle( rset.getString("BOARD_TITLE") );
+				board.setBoardContent( rset.getString("BOARD_CONTENT") );
+				board.setMemberId( rset.getString("MEMBER_ID") );
+				board.setReadCount( rset.getInt("READ_COUNT") );
+				board.setBoardCreateDate( rset.getTimestamp("BOARD_CREATE_DT"));
+				board.setBoardModifyDate( rset.getTimestamp("BOARD_MODIFY_DT"));
+				board.setCategoryName( rset.getString("CATEGORY_NM") );
 			}
-	
+			
+			
 		}finally {
 			close(rset);
 			close(pstmt);
-			
-			
 		}
 		
-		
-		
-		return fBoard;
+		return board;
 	}
-
-	/** 게시물 조회수 증가
-	 * @param conn
-	 * @param fBoardNo
-	 * @return
-	 * @throws Exception
-	 */
-	public int increaseReadCount(Connection conn, int fBoardNo) throws Exception {
-		
-		int result = 0;
-		
-		String query = prop.getProperty("increaseReadCount");
-		
-				try {
+	
+	
+	/** 조회수 증가 DAO
+	    * @param conn
+	    * @param boardNo
+	    * @return   result
+	    * @throws Exception
+	    */
+	   public int increaseReadCount(Connection conn, int boardNo) throws Exception {
+	      int result =0;
+	      
+	      String query = prop.getProperty("increaseReadCount");
+	      
+	      try {
 	         
 	         pstmt = conn.prepareStatement(query);
-	         pstmt.setInt(1,fBoardNo);
+	         pstmt.setInt(1,boardNo);
 	         
 	         result = pstmt.executeUpdate();
-	     
-				}finally {
-	       
-	    	  close(pstmt);
+	      }finally {
+	         close(pstmt);
 	      }
 	      
 	      return result;
 	   }
 
+
+	   
 	/** 다음 게시글 번호 조회 DAO
 	 * @param conn
 	 * @return boardNo
 	 * @throws Exception
 	 */
-	public int selectFNextNo(Connection conn) throws Exception {
+	public int selectNextNo(Connection conn) throws Exception{
+		int boardNo = 0;
 		
-		int fBoardNo = 0;
-
-		String query = prop.getProperty("selectFNextNo");
-
+		String query = prop.getProperty("selectNextNo");
+		
 		try {
-
 			stmt = conn.createStatement();
-
 			rset = stmt.executeQuery(query);
-
-			if (rset.next()) {
-				fBoardNo = rset.getInt(1);
+			
+			if(rset.next()) {
+				boardNo = rset.getInt(1);
 			}
-
-		} finally {
+			
+		}finally {
 			close(rset);
 			close(stmt);
 		}
-
-		return fBoardNo;
+		
+		return boardNo;
 	}
+
 
 	/** 게시글 삽입 DAO
 	 * @param conn
@@ -236,55 +209,57 @@ public class FreeBoardDAO {
 	 * @return result
 	 * @throws Exception
 	 */
-	public int insertFBoard(Connection conn, Map<String, Object> map) throws Exception {
-		
+	public int insertBoard(Connection conn, Map<String, Object> map) throws Exception{
 		int result = 0;
-
-		String query = prop.getProperty("insertFBoard");
-
+		
+		String query = prop.getProperty("insertBoard");
+		
 		try {
-
 			pstmt = conn.prepareStatement(query);
-			pstmt.setInt(1, (int) map.get("fBoardNo"));
-			pstmt.setString(2, (String) map.get("fBoardTitle"));
-			pstmt.setString(3, (String) map.get("fBoardContent"));
-			pstmt.setInt(4, (int) map.get("memNo"));
-
-			result = pstmt.executeUpdate();
-
-		} finally {
 			
+			pstmt.setInt(1, (int)map.get("boardNo")  );
+			pstmt.setString(2, (String)map.get("boardTitle")  );
+			pstmt.setString(3, (String)map.get("boardContent")  );
+			pstmt.setInt(4, (int)map.get("boardWriter")  );
+			pstmt.setInt(5, (int)map.get("categoryCode")  );
+			
+			result = pstmt.executeUpdate();
+		}finally {
 			close(pstmt);
-
-		}
+		} 
+		
 		return result;
 	}
+
 
 	/** 파일 정보 삽입 DAO
 	 * @param conn
 	 * @param at
-	 * @return result 
+	 * @return result
 	 * @throws Exception
 	 */
-	public int insertAttachment(Connection conn, FRAttachment at) throws Exception {
+	public int insertAttachment(Connection conn, Attachment at) throws Exception {
 		int result = 0;
-
+		
 		String query = prop.getProperty("insertAttachment");
-
+		
 		try {
 			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, at.getImgPath());
-			pstmt.setString(2, at.getImgName());
-			pstmt.setInt(3, at.getImgLevel());
-			pstmt.setInt(4, at.getfBoardNo());
-
+			
+			pstmt.setString(1, at.getFilePath());
+			pstmt.setString(2, at.getFileName());
+			pstmt.setInt(3, at.getFileLevel());
+			pstmt.setInt(4, at.getParentBoardNo());
+			
 			result = pstmt.executeUpdate();
-
+			
 		} finally {
 			close(pstmt);
 		}
+		
 		return result;
 	}
+
 
 	/** 게시글에 포함된 이미지 목록 조회 DAO
 	 * @param conn
@@ -292,171 +267,146 @@ public class FreeBoardDAO {
 	 * @return fList
 	 * @throws Exception
 	 */
-	public List<FRAttachment> selectFBoardFiles(Connection conn, int fBoardNo) throws Exception {
-		List<FRAttachment> fileList = null;
-
+	public List<Attachment> selectBoardFiles(Connection conn, int boardNo) throws Exception {
+		
+		List<Attachment> fList = null;
+		
 		String query = prop.getProperty("selectBoardFiles");
-
+		
 		try {
 			pstmt = conn.prepareStatement(query);
-
-			pstmt.setInt(1, fBoardNo);
+			
+			pstmt.setInt(1, boardNo);
 
 			rset = pstmt.executeQuery();
-
-			fileList = new ArrayList<FRAttachment>();
-
-			while (rset.next()) {
-
-				FRAttachment at = new FRAttachment(rset.getInt("FR_IMG_NO"), rset.getString("FR_IMG_NM"),
-						rset.getInt("FR_IMG_LV"));
-
-				at.setImgPath(rset.getString("FR_PATH"));
-				fileList.add(at);
+			
+			fList = new ArrayList<Attachment>();
+			
+			while(rset.next()) {
+				Attachment at = new Attachment(
+							rset.getInt("FILE_NO"), 
+							rset.getString("FILE_NAME"), 
+							rset.getInt("FILE_LEVEL")) ;
+			
+				at.setFilePath( rset.getString("FILE_PATH")  );
+				
+				fList.add(at);
 			}
-
-		} finally {
+			
+		}finally {
 			close(rset);
 			close(pstmt);
-
 		}
-
-		return fileList;
+		
+		
+		return fList;
 	}
+
 
 	/** 썸네일 목록 조회 DAO
 	 * @param conn
-	 * @param fPInfo
+	 * @param pInfo
 	 * @return fList
 	 * @throws Exception
 	 */
-	public List<FRAttachment> selectThumbnailList(Connection conn, FreePageInfo fPInfo) throws Exception {
-		List<FRAttachment> fileList = null;
-
+	public List<Attachment> selectThumbnailList(Connection conn, PageInfo pInfo) throws Exception{
+		List<Attachment> fList = null;
+		
+		
 		String query = prop.getProperty("selectThumbnailList");
-
+		
 		try {
-//			위치 홀더에 들어갈 시작행 , 끝 행번호 계산
-
-			int startRow = (fPInfo.getCurrentPage() - 1) * fPInfo.getLimit() + 1;
-			int endRow = startRow + fPInfo.getLimit() - 1;
-
+			// 위치 홀더에 들어갈 시작 행, 끝 행번호 계산
+			int startRow = (pInfo.getCurrentPage() -1) * pInfo.getLimit() + 1;
+			int endRow = startRow + pInfo.getLimit() - 1;
+			
 			pstmt = conn.prepareStatement(query);
-
 			pstmt.setInt(1, startRow);
 			pstmt.setInt(2, endRow);
-
+			
 			rset = pstmt.executeQuery();
-
-//			조회 결과를 저장할 List 생성
-			fileList = new ArrayList<FRAttachment>();
-			while (rset.next()) {
-
-				FRAttachment at = new FRAttachment();
-				at.setImgName(rset.getString("FR_IMG_NM"));
-				at.setfBoardNo(rset.getInt("FR_NO"));
-
-				fileList.add(at);
+			
+			// 조회 결과를 저장할 List 생성
+			fList = new ArrayList<Attachment>();
+			
+			while(rset.next()) {
+				
+				Attachment at = new Attachment();
+				at.setFileName(rset.getString("FILE_NAME"));
+				at.setParentBoardNo(rset.getInt("PARENT_BOARD_NO"));
+				
+				fList.add(at);
 			}
-
-		} finally {
+			
+		}finally {
 			close(rset);
 			close(pstmt);
-
 		}
-
-		return fileList;
+		
+		return fList;
 	}
-	
+
+
 	/** 게시글 수정 DAO
 	 * @param conn
 	 * @param map
 	 * @return result
 	 * @throws Exception
 	 */
-	public int updateFBoard(Connection conn, Map<String, Object> map) throws Exception {
+	public int updateBoard(Connection conn, Map<String, Object> map) throws Exception{
+	
 		int result = 0;
-
-		String query = prop.getProperty("updateFBoard");
-
+		
+		String query = prop.getProperty("updateBoard");
+		
 		try {
-
 			pstmt = conn.prepareStatement(query);
-
-			pstmt.setString(1, (String) map.get("fBoardTitle"));
-			pstmt.setString(2, (String) map.get("fBoardContent"));
-			pstmt.setInt(3, (int) map.get("fBoardNo"));
-
+			
+			pstmt.setString( 1, (String)map.get("boardTitle") );
+			pstmt.setString( 2, (String)map.get("boardContent") );
+			pstmt.setInt( 3, (int)map.get("categoryCode") );
+			pstmt.setInt( 4, (int)map.get("boardNo") );
+			
 			result = pstmt.executeUpdate();
-
+		
 		} finally {
-
 			close(pstmt);
-
 		}
-
+		
 		return result;
 	}
+
 
 	/** 파일 정보 수정 DAO
 	 * @param conn
 	 * @param newFile
-	 * @return result 
-	 * @throws Exception
-	 */
-	public int updateAttachment(Connection conn, FRAttachment newFile) throws Exception {
-		int result = 0;
-
-		String query = prop.getProperty("updateAttachment");
-
-		try {
-			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, newFile.getImgPath());
-			pstmt.setString(2, newFile.getImgName());
-			pstmt.setInt(3, newFile.getImgNo());
-
-			result = pstmt.executeUpdate();
-		} finally {
-			close(pstmt);
-		}
-		return result;
-	}
-
-	/** 게시물 삭제 DAO
-	 * @param conn
-	 * @param boardNo
 	 * @return result
 	 * @throws Exception
 	 */
-	public int deleteFBoard(Connection conn, int fBoardNo) throws Exception {
+	public int updateAttachment(Connection conn, Attachment newFile) throws Exception {
 		
 		int result = 0;
-		
-		String query = prop.getProperty("deleteFBoard");
+		String query = prop.getProperty("updateAttachment");
 		
 		try {
-			
 			pstmt = conn.prepareStatement(query);
 			
-			pstmt.setInt(1, fBoardNo);
+			pstmt.setString(1, newFile.getFilePath());
+			pstmt.setString(2, newFile.getFileName());
+			pstmt.setInt(3, newFile.getFileNo());
 			
 			result = pstmt.executeUpdate();
 			
 		}finally {
-			
 			close(pstmt);
-			
-			
 		}
-		
-		
-		
 		
 		return result;
 	}
-
-
+	
+	
+	
+	
+	
+	
 }
-
-
-
